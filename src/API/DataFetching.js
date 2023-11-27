822b32502717b77f809bfcba50438ebfaee6052a
Minhaj I K <minhajismailkhan@gmail.com>
2023-11-27 17:23:11 +0530

    corrected version of fetching data from api

--- src/API/DataFetching.js
+++ src/API/DataFetching.js
@@ -7,7 +7,7 @@ const DataFetching = () => {
     useEffect(()=>{
         fetch('https://jsonplaceholder.typicode.com/posts')
       .then(response => response.json())
-      .then(possts => setPosts(posts.splice(0,10)))   
+      .then(posts => setPosts(posts.splice(0,10)))   
     }, [])
 
   return (
